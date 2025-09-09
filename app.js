aea8ba3e2da4ef682ab34230266e408bb0d6c1a7
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-09-09 11:24:43 -0300

    Adicionando busca de imagens pelo nome da raça do cachorro.

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ async function buscarImagens(raca) {
     const url = `https://dog.ceo/api/breed/${raca}/images`
     const response = await fetch(url)
     const data = await response.json()
-    
-    
+
+    return data.message
 }
 
-function carregarImagens(imagens) {
+
+function criarImagem(imagem) {
+
     const galeria = document.getElementById('container-imagens')
 
-    const imagemDog = document.createElement('img')
-    
-    imagemDog.src = imagens.add(imagens[i])
+    const dogImage = document.createElement('img')
+    dogImage.src = imagem
 
+    galeria.appendChild(dogImage)
 }
 
-buscarImagens('doberman')
+
+async function carregarImagens(raca) {
+    const imagens = await buscarImagens(raca)
+
+    imagens.forEach(criarImagem)
+}
+
+const botao = document.getElementById('buscar')
+
+botao.addEventListener('click', () => {
+    const raca = document.getElementById('input').value
+    carregarImagens(raca)
+})
